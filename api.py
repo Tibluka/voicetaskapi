4cfff0846bd16341ed5eabfc4e90666c4558269f
Lucas <P0659387@portoseguro.com.br>
2025-08-05 10:50:42 -0300

    a

--- api.py
+++ api.py
@@ -8,28 +8,27 @@ from routes.config_route import config_bp
 from routes.execute_route import execute_bp
 from routes.projects_route import projects_bp
 from routes.fixed_bills_route import fixed_bills_bp  # Nova importação
-from routes.summary_route import summary_bp  # Nova importação
-from db.mongo import client
+from routes.summary_route import summary_bp  # Nova importação 
+from db.mongo import client 
 
-app = Flask(__name__)
-CORS(app)
-
+app = Flask(__name__) 
+CORS(app) 
+ 
 try:
-    client.admin.command("ping")
-    print("✅ Conexão com o MongoDB estabelecida com sucesso.")
-except Exception as e:
-    print("❌ Erro ao conectar ao MongoDB:", e)
-    sys.exit(1)
+    client.admin.command("ping") 
+    print("✅ Conexão com o MongoDB estabelecida com sucesso.") 
+except Exception as e: 
+    print("❌ Erro ao conectar ao MongoDB:", e) 
+    sys.exit(1) 
+ 
+app.register_blueprint(execute_bp) 
+app.register_blueprint(transcribe_bp) 
+app.register_blueprint(auth_bp) 
+app.register_blueprint(spending_bp) 
+app.register_blueprint(config_bp) 
+app.register_blueprint(projects_bp) 
+app.register_blueprint(fixed_bills_bp)  # Nova rota 
+app.register_blueprint(summary_bp)  # Nova rota 
 
-
-app.register_blueprint(execute_bp)
-app.register_blueprint(transcribe_bp)
-app.register_blueprint(auth_bp)
-app.register_blueprint(spending_bp)
-app.register_blueprint(config_bp)
-app.register_blueprint(projects_bp)
-app.register_blueprint(fixed_bills_bp)  # Nova rota
-app.register_blueprint(summary_bp)  # Nova rota
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=6002)
+if __name__ == "__main__": 
+    app.run(debug=True, host="0.0.0.0", port=6002) 
